bb73874e0f1a32bac3fabeaff02fa14cb5ac1a4c
Sean Olson <solson6@student.rccd.edu>
2025-10-12 08:32:32 -0700

    debug reference the external function from main.c

--- code_samples/multi_file_program/main.c
+++ code_samples/multi_file_program/main.c
@@ -8,7 +8,7 @@ int main(void) {
     printf("Hello from main.c\n");
     printf("Calling int_from_asm_a() in the_asm_file.asm\n");
 
-    long asm_a_value = manager(); // assign return value from assembly
+    long asm_a_value = int_from_asm_a(); // assign return value from assembly
 
     printf("\back in main.c and we received this from the_asm_file.asm: %ld\n", asm_a_value);
     printf("Main will return 0 to the operating system. Bye!\n");
