a57bc380e5fa465b850f33ad890435995a3eaf80
Sean Olson <solson6@student.rccd.edu>
2025-10-12 12:11:08 -0700

    clean up formatting in manager.asm

--- code_samples/multi_file_program/manager.asm
+++ code_samples/multi_file_program/manager.asm
@@ -10,8 +10,8 @@ section					.data
 
 	; ---------------------------
 	; strings
-	MSG_GREET												db 			"In the_asm_file.asm"
-	MSG_GREET_LEN										equ			$-MSG_GREET
+	MSG_GREET														db 			"In the_asm_file.asm"
+	MSG_GREET_LEN												equ			$-MSG_GREET
 
 
 	MSG_CALLING_DISPLAY_ARRAY						db 			"Calling display_array() in another assembly-code file."
@@ -26,25 +26,22 @@ section					.data
 	MSG_RETURN_FROM_REVERSE_ARRAY				db 			"Returned to manager.asm from calling reverse_array() function in a C++ code file."
 	MSG_RETURN_FROM_REVERSE_ARRAY_LEN		equ			$-MSG_RETURN_FROM_REVERSE_ARRAY		
 
-	MSG_GOOD_BYE										db 			"Leaving the_asm_file.asm.  Good bye!"
-	MSG_GOOD_BYE_LEN								equ			$-MSG_GOOD_BYE
-
-	MSG_CRLF												db 			13, 10
-	MSG_CRLF_LEN										equ			$-MSG_CRLF
+	MSG_GOOD_BYE												db 			"Leaving the_asm_file.asm.  Good bye!"
+	MSG_GOOD_BYE_LEN										equ			$-MSG_GOOD_BYE
 
 	; ---------------------------
 	; system calls
-	SYS_WRITE												equ			1
+	SYS_WRITE														equ			1
 
 	; ---------------------------
 	; file descriptors
-	FD_STDOUT												equ			1
+	FD_STDOUT														equ			1
 
 	; ---------------------------
 	; debug data
-	INT_VALUE											dq			100
-	INT_ARRAY 										dq 			10, 20, 30, 40, 50 
-	INT_ARRAY_LEN									dq			5	
+	INT_VALUE														dq			100
+	INT_ARRAY 													dq 			10, 20, 30, 40, 50 
+	INT_ARRAY_LEN												dq			5	
 
 ; ----------------------------------------------------------
 ;  text section
@@ -119,53 +116,3 @@ section .text
 		; set return value
 		mov rax, [INT_VALUE]
 		ret
-
-
-	; ---------------------------
-	; void print_string()
-	;
-	;	register usage
-	;		r8:	string to be printed
-	;		r9:	string length
-	; print_string:		
-
-	; 	; ------------
-	; 	; preserve - none
-
-	; 	; ------------
-	; 	; grab the arguments	
-	; 	mov r8,	rdi
-	; 	mov	r9,	rsi
-		
-	; 	; ------------
-	; 	; set the arguments and make the syscall			
-	; 	mov rax,	SYS_WRITE
-	; 	mov	rdi,	FD_STDOUT
-	; 	mov	rsi,	r8
-	; 	mov	rdx,	r9
-	; 	syscall
-
-	; 	; ------------
-	; 	; restore - none
-
-	; 	ret
-
-	; ; ---------------------------
-	; ; void print_newline()
-	; print_newline:	
-		
-	; 	; ------------
-	; 	; preserve - none
-		
-	; 	; ------------
-	; 	; set the arguments and make the syscall			
-	; 	mov rax,	SYS_WRITE
-	; 	mov	rdi,	FD_STDOUT
-	; 	mov	rsi,	MSG_CRLF
-	; 	mov	rdx,	MSG_CRLF_LEN
-	; 	syscall
-
-	; 	; ------------
-	; 	; restore - none
-
-	; 	ret	
