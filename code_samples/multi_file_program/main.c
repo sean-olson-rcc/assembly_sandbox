3d6bab7b2430a7449e6f72f91f41d79df8f52744
Sean Olson <SloughHand@gmail.com>
2025-10-12 16:18:56 -0700

    update messaging in main.c

--- code_samples/multi_file_program/main.c
+++ code_samples/multi_file_program/main.c
@@ -5,12 +5,12 @@
 extern long manager(void);
 
 int main(void) {
-    printf("Hello from main.c\n");
-    printf("Calling int_from_asm_a() in the_asm_file.asm\n");
+    printf("Welcome to Arrays of Integers\n");
+    printf("Brought to you by Sean Olson\n");
 
-    long asm_a_value = manager(); // assign return value from assembly
+    long mean_value = manager(); // assign return value from assembly
 
-    printf("\nback in main.c and we received this from the_asm_file.asm: %ld\n", asm_a_value);
+    printf("\n Main received this number %ld.\n", mean_value);
     printf("Main will return 0 to the operating system. Bye!\n");
 
     return 0;
