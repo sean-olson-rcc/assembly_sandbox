f8cb79c4d439a648297a084b5ce4c20201ff0141
Sean Olson <SloughHand@gmail.com>
2025-10-12 12:27:35 -0700

    update main.c reference to manager.asm

--- code_samples/multi_file_program/main.c
+++ code_samples/multi_file_program/main.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 // external function in the
-extern long int_from_asm_a(void);
+extern long manager(void);
 
 int main(void) {
     printf("Hello from main.c\n");
     printf("Calling int_from_asm_a() in the_asm_file.asm\n");
 
-    long asm_a_value = int_from_asm_a(); // assign return value from assembly
+    long asm_a_value = manager(); // assign return value from assembly
 
     printf("\nback in main.c and we received this from the_asm_file.asm: %ld\n", asm_a_value);
     printf("Main will return 0 to the operating system. Bye!\n");
