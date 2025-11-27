9276e48b08bc342649fd1d9d527dca2b79c36a9c
Sean Olson <ieSean@hotmail.com>
2025-11-27 13:56:29 -0800

    update assembly makefiles to use dynamic linker flags

--- _makefile_templates/ubuntu/asm_with_c_lib/main.asm
+++ _makefile_templates/ubuntu/asm_with_c_lib/main.asm
@@ -12,6 +12,11 @@
 
 section     .data
 
+; db is a byte type variable that is held in memory. 
+; it's a pointer, pointing to the first character in the string
+HELLO_STRING    db     "Hello, my name is Sean Olson.",13,10,"This is an assembly-language program that uses the C-Library.",13,10,0
+
+
 SYS_WRITE       equ     1
 SYS_EXIT        equ     60
 
@@ -21,9 +26,6 @@ FD_STDIN        equ     0 ; define the standard input
 FD_STDOUT       equ     1 ; define the standard output
 FD_STDERR       equ     2 ; define the standard error
 
-; db is a byte type variable that is held in memory. 
-; it's a pointer, pointing to the first character in the string
-HELLO_STRING    db     "Hello, my name is Sean Olson.",13,10,"This is an assembly-language program that uses the C-Library.",13,10,0
 
 ;------------------------------------------------------------------------------
 ; bss section for unitialized global and static variables
@@ -56,3 +58,6 @@ main:
     mov rax, SYS_EXIT
     mov rdi, EXIT_SUCCESS
     syscall
+
+
+section .note.GNU-stack noalloc noexec nowrite progbits
