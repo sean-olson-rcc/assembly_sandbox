e2f20850296d46b10ba4af5fde1c757f51ab9463
Sean Olson <ieSean@hotmail.com>
2025-12-14 12:34:24 -0800

    add template for function boilerplate

---
+++ code_module_templates/ubuntu/function_boiler_plate.asm
@@ -0,0 +1,26 @@
+;--------------------------------------
+;	void fn()
+;		register usage:
+;			rbp: stack frame base pointer preserved
+;			rsp: stack pointer used to adjust 16-byte stack alignment
+;
+fn:
+	;-------------------
+	; prologue
+	push rbp
+	mov rbp, rsp
+
+	;-------------------
+	; set 16-byte stack alignment
+	sub rsp, 16
+	and rsp, -16
+
+
+
+	;-------------------
+	; epilogue
+
+	mov rsp, rbp	
+	pop rbp
+
+	ret
